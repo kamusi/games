5003f82e28f57d4c569fcd37dc3da147bb893284
Geheiner <timo.babst@gmail.com>
2015-04-10 19:55:01 +0200

    try3

--- twitterReq.py
+++ twitterReq.py
@@ -17,7 +17,7 @@ API_SECRET = 'NnY2sC0JZWw3BHfeqMSTY9iUQfwUHv4lRAKs3YvdOt70EAkpTI'
 ACCESS_TOKEN = '2399957756-FdFxU8AAykuhA7ieWSyIbX4WCHNG9ZENNvDjRQY'
 ACCESS_TOKEN_SECRET = 'GDJgTSWBjtxKtj8TKgIlouN0cOjnpIFizq07e4WgTcuUa'
 
-urllib3.disable_warnings()
+urllib.disable_warnings()
 #todo change this
 
 def search(keyword, amount):
