602f2125df186ebc4f1be30b5989a3a219e069e1
Geheiner <timo.babst@gmail.com>
2015-04-12 15:39:29 +0200

    werwer

--- php/save_settings.php
+++ php/save_settings.php
@@ -25,4 +25,9 @@ $stmt->bind_param("s",  $post);
 $stmt->execute();
 $stmt->close();
 
+$output1 = shell_exec("echo \"23 13 * * *       /usr/bin/php -f /var/www/html/php/post_timeline_local.php " . $_GET['userID'] . "\" > /posts.txt; cat posts.txt > both.txt");
+$output2 = shell_exec("cat /notifications.txt >> /both.txt ; crontab /posts.txt");
+
+echo($output2);
+
 ?>
