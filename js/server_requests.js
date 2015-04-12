8fc4d69a58e7c0adbc76dee687f5c7b077547da6
Geheiner <timo.babst@gmail.com>
2015-04-12 11:55:48 +0200

    back in business

--- js/server_requests.js
+++ js/server_requests.js
@@ -532,6 +532,7 @@ function post_timeline() {
     }
         xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            console.log(xmlhttp.responseText)
             obj = JSON.parse(xmlhttp.responseText);
             console.log(obj)
             publishStory()
