e085ec3daa4f88172aa85efcc37e0c13ddcc8125
Geheiner <timo.babst@gmail.com>
2015-04-13 14:54:56 +0200

    monday debug

---
+++ php/notification_tweet.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "facebook-php-sdk/facebook.php";
+
+$userID = $_GET['userID'];
+
+$facebook = new Facebook();
+
+$app_id = YOUR_APP_ID;
+
+$app_secret = YOUR_APP_SECRET;
+
+$app_access_token = $app_id . '|' . $app_secret;
+
+$response = $facebook->api( '/' . $userID . '/notifications', 'POST', array(
+
+	'template' => 'You have received a new message.',
+
+	'href' => 'RELATIVE URL',
+
+	'access_token' => $app_access_token
+	) );    
+
+print_r($response);
+?>
