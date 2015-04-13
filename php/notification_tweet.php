16c508e65db3723a0da73facf4ead2ade885006e
Geheiner <timo.babst@gmail.com>
2015-04-13 22:20:46 +0200

    monday evening

--- php/notification_tweet.php
+++ php/notification_tweet.php
@@ -38,34 +38,6 @@ use Facebook\Entities\AccessToken;
 use Facebook\Entities\SignedRequest;
 
  
-// init app with app id (APPID) and secret (SECRET)
-FacebookSession::setDefaultApplication('APPID','SECRET');
- 
-// login helper with redirect_uri
-$helper = new FacebookRedirectLoginHelper( 'http://www.metah.ch/' );
- 
-try {
-  $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
-  // When Facebook returns an error
-} catch( Exception $ex ) {
-  // When validation fails or other local issues
-}
- 
-// see if we have a session
-if ( isset( $session ) ) {
-  // graph api request for user data
-  $request = new FacebookRequest( $session, 'GET', '/me' );
-  $response = $request->execute();
-  // get response
-  $graphObject = $response->getGraphObject();
-   
-  // print data
-  echo  print_r( $graphObject, 1 );
-} else {
-  // show login url
-  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
-}
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
