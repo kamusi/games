81d4b6f8332dea9fcbff66271d928182171b8099
Geheiner <timo.babst@gmail.com>
2015-04-13 14:52:18 +0200

    monday debug

--- js/server_requests.js
+++ js/server_requests.js
@@ -187,7 +187,7 @@ function submitTweets() {
         publishStory(1)
     }
     else {
-        post_timeline()
+        post_timeline();
     }
 
 }
@@ -555,11 +555,31 @@ function post_timeline() {
                 console.log("No activity to post")
             }
             else {
+                trigger_notification()
                 publishStory(obj)
             }
         }
     }
 
     xmlhttp.open("GET","php/post_timeline.php?userID=" + userID); 
+    xmlhttp.send();   
+}
+
+function trigger_notification() {
+  var xmlhttp;
+  
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            console.log("NOTIFICATION SHOULD HAVE BEEN TRIGGERED BY NOW")
+        }
+    }
+
+    xmlhttp.open("GET","php/notification_tweet.php?userID=" + userID); 
     xmlhttp.send();   
 }
