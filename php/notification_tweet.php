9493b2095e3c03a06af417b2750b93e38a7d858e
Geheiner <timo.babst@gmail.com>
2015-04-14 00:09:38 +0200

    monday evening

--- php/notification_tweet.php
+++ php/notification_tweet.php
@@ -74,7 +74,7 @@ $stmt->execute();
 $stmt->close();
 
 if($newPoints == 0){
-	echo "Did not sent notif, no points were gained.";
+	echo "Did not sent notif, no points were gained." .$userID ;
 }
 else {
 	FacebookSession::setDefaultApplication($app_id, $app_secret);
