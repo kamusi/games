5d9814704bc161a6e82c7798b337dd88f0b4218e
Geheiner <timo.babst@gmail.com>
2015-04-19 15:05:10 +0200

    debugging previous stuff

--- index.php
+++ index.php
@@ -61,9 +61,10 @@ $newToken = generateToken();
 						
 							</div>							
 							<table id="definitions">
-									<img title="Next" id="next1" ng-click="clear($event)" class="control" src="media/rightarrow.png" onclick="playClick();vote();get_ranked();">
-						
+
 								<tr><td>
+										<img title="Next" id="next1" ng-click="clear($event)" class="control" src="media/rightarrow.png" onclick="playClick();vote();get_ranked();">
+						
 									<li ng-click="toggleTooltip($event)" id="user_definition" class="inactive_definition">{{value}}</li>
 								</td></tr>
 							</table>
