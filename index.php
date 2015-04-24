ee8522bf8aeb1440c66c8323b4fec3f2b0fe7545
Geheiner <timo.babst@gmail.com>
2015-04-24 16:09:44 +0200

    friday2

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ $newToken = generateToken();
 							</div>
 							<table id="translations">
 								<tr><td>
-								<img title="Submit" id="SubmitTrans" ng-click="clear($event)" class="control" src="media/rightarrow.png" onclick="playClick();;soumettre_traduction();get_ranked_mode_2();">
+								<img title="Submit" id="SubmitTrans" ng-click="clear($event)" class="controlSmall" src="media/rightarrowSmall.png" onclick="playClick();;soumettre_traduction();get_ranked_mode_2();">
 						</div>	
 									<li ng-click="toggleTooltip2($event)" id="user_translation" class="inactive_definition">{{translation}}</li>
 								</td></tr>
