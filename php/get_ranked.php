b980650a8af57f5af94c704a64a1f26c028684d4
Geheiner <timo.babst@gmail.com>
2015-04-25 17:03:45 +0200

    saturday1

--- php/get_ranked.php
+++ php/get_ranked.php
@@ -22,6 +22,7 @@ if (!$con) {
 
 $mysqli = new mysqli('localhost', $user, $pass, $db);
 
+//fetch the user in order to see which word is for him
 $stmt = $mysqli->prepare("SELECT * FROM users WHERE UserID = ? ");
 $stmt->bind_param("s", $userID );
 $stmt->execute();
@@ -31,6 +32,28 @@ $stmt->close();
 
 $row = $result->fetch_assoc();
 $user_position = $row["PositionMode1"];
+$user_offset = $row["OffsetMode1"];
+
+//fetch the word that has as rank user s position+offset
+$sql =  "SELECT ID As ID, DefinitionID As DefinitionID, Rank As Rank FROM (";
+$sql.=	"SELECT w.ID, w.DefinitionID, r.Rank FROM rankedwords As r LEFT JOIN words As w ON r.Word = w.Word";
+$sql.=	") As sq WHERE sq.ID IS NOT NULL  AND sq.Rank = ? ;";
+
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", intval($user_position) + intval($user_offset) );
+$stmt->execute();
+$result = $stmt->get_result();
+
+
+$stmt->close();
+
+$sql =  "SELECT sq.ID As WordID, sq.Word, sq.PartOfSpeech, d.ID As DefinitionID, d.Definition, d.GroupID, d.UserID As Author ";
+$sql .= "FROM (SELECT * FROM words WHERE ID=" . $word_id . ") AS sq ";
+$sql .= "LEFT JOIN definitions As d ON sq.DefinitionID = d.GroupID";
+$sql .= "WHERE d.GroupID NOT IN (SELECT GroupID FROM usersDefinitionsMode1 WHERE userID= ?) ORDER BY Votes desc;";
+
+
 
 
 $jsonData = json_encode($user_position);
