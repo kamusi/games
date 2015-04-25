27c59cd6b0d46d876d14da5b995a32ebb32ef1d7
Geheiner <timo.babst@gmail.com>
2015-04-25 22:25:00 +0200

    saturday1

--- php/get_ranked.php
+++ php/get_ranked.php
@@ -24,31 +24,12 @@ if (!$con) {
 $mysqli = new mysqli('localhost', $user, $pass, $db);
 
 
+$results_array = FALSE;
 
+while($results_array === FALSE) {
 $word_id =lookForWord($userID, $mysqli); 
-
-$sql =  "SELECT sq.ID As WordID, sq.Word, sq.PartOfSpeech, d.ID As DefinitionID, d.Definition, d.GroupID, d.UserID As Author ";
-$sql .= "FROM (SELECT * FROM words WHERE ID=?) AS sq ";
-$sql .= "LEFT JOIN definitions As d ON sq.DefinitionID = d.GroupID";
-$sql .= " ORDER BY Votes desc;";
-
-$stmt = $mysqli->prepare($sql);
-if ($stmt === FALSE) {
-	die ("Mysql Error: " . $mysqli->error);
+$results_array = getDefinitions($word_id, $mysqli);
 }
-
-$stmt->bind_param("i",  $word_id);
-$stmt->execute();
-$result = $stmt->get_result();
-
-
-while ($row = $result->fetch_assoc()) {
-	$results_array[] = $row;
-}
-
-
-$stmt->close();
-
 
 $jsonData = json_encode($results_array);
 echo $jsonData;
@@ -72,59 +53,89 @@ function lookForWord($userID, $mysqli) {
 		$sql.=	"SELECT w.ID, w.DefinitionID, r.Rank FROM rankedwords As r LEFT JOIN words As w ON r.Word = w.Word";
 		$sql.=	") As sq WHERE sq.ID IS NOT NULL AND sq.DefinitionID IS NOT NULL AND sq.ID NOT IN (SELECT WordID FROM wordsAlreadySeenMode1 WHERE UserID=?) AND sq.Rank = ? LIMIT 1;";
 
-		$sum = intval($user_position) + intval($user_offset);
+$sum = intval($user_position) + intval($user_offset);
 
-		$stmt = $mysqli->prepare($sql);
-		if ($stmt === FALSE) {
-			die ("Mysql Error: " . $mysqli->error);
-		}
+$stmt = $mysqli->prepare($sql);
+if ($stmt === FALSE) {
+	die ("Mysql Error: " . $mysqli->error);
+}
 
-		$stmt->bind_param("si", $userID, $sum);
+$stmt->bind_param("si", $userID, $sum);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$word_id = $row["ID"];
+
+$stmt->close();
+if($result-> num_rows === 0){
+	$stmt->close();
+	if($user_offset == 0) {
+		$stmt = $mysqli->prepare("UPDATE users SET PositionMode1 = PositionMode1 + 1 WHERE UserID=?;");
+		$stmt->bind_param("s", $userID);
 		$stmt->execute();
-		$result = $stmt->get_result();
-			$row = $result->fetch_assoc();
-			$word_id = $row["ID"];
-
-			$stmt->close();
-		if($result-> num_rows === 0 || $word_id == null){
-			$stmt->close();
-			if($user_offset == 0) {
-				$stmt = $mysqli->prepare("UPDATE users SET PositionMode1 = PositionMode1 + 1 WHERE UserID=?;");
-				$stmt->bind_param("s", $userID);
-				$stmt->execute();
-				$stmt->close();
-			}
-			else {
-				$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = OffsetMode1 + 1 WHERE UserID=?;");
-				$stmt->bind_param("s", $userID);
-				$stmt->execute();
-				$stmt->close();		
-			}
-			return lookForWord($userID, $mysqli);
-		}
-		else {
+		$stmt->close();
+	}
+	else {
+		$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = OffsetMode1 + 1 WHERE UserID=?;");
+		$stmt->bind_param("s", $userID);
+		$stmt->execute();
+		$stmt->close();		
+	}
+	return lookForWord($userID, $mysqli);
+}
+else {
 
 
 
-			$stmt = $mysqli->prepare("INSERT INTO wordsAlreadySeenMode1 (UserID ,WordID) VALUES (?,?);");
-			$stmt->bind_param("si", $userID, $word_id);
-			$stmt->execute();
-			$stmt->close();	
-			if($user_offset > $offsetModulo){
-				$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = 0 WHERE UserID=?;");
-				$stmt->bind_param("s", $userID);
-				$stmt->execute();
-				$stmt->close();
-			}
-			else {
+	$stmt = $mysqli->prepare("INSERT INTO wordsAlreadySeenMode1 (UserID ,WordID) VALUES (?,?);");
+	$stmt->bind_param("si", $userID, $word_id);
+	$stmt->execute();
+	$stmt->close();	
+	if($user_offset > $offsetModulo){
+		$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = 0 WHERE UserID=?;");
+		$stmt->bind_param("s", $userID);
+		$stmt->execute();
+		$stmt->close();
+	}
+	else {
 
-				$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = OffsetMode1 + 1 WHERE UserID=?;");
-				$stmt->bind_param("s", $userID);
-				$stmt->execute();
-				$stmt->close();	
-			}	
-			return $word_id;
-		}
+		$stmt = $mysqli->prepare("UPDATE users SET OffsetMode1 = OffsetMode1 + 1 WHERE UserID=?;");
+		$stmt->bind_param("s", $userID);
+		$stmt->execute();
+		$stmt->close();	
+	}	
+	return $word_id;
+}
+}
+
+function getDefinitions($word_id, $mysqli){
+	$sql =  "SELECT sq.ID As WordID, sq.Word, sq.PartOfSpeech, d.ID As DefinitionID, d.Definition, d.GroupID, d.UserID As Author ";
+	$sql .= "FROM (SELECT * FROM words WHERE ID=?) AS sq ";
+	$sql .= "LEFT JOIN definitions As d ON sq.DefinitionID = d.GroupID WHERE d.GroupID IS NOT NULL";
+	$sql .= " ORDER BY Votes desc;";
+
+	$stmt = $mysqli->prepare($sql);
+	if ($stmt === FALSE) {
+		die ("Mysql Error: " . $mysqli->error);
 	}
 
-	?>
+	$stmt->bind_param("i",  $word_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if($result-> num_rows === 0){
+		return FALSE;
+	}
+	else {
+
+		while ($row = $result->fetch_assoc()) {
+			$results_array[] = $row;
+		}
+
+
+		$stmt->close();
+		return $results_array
+	}
+}
+
+?>
