90483e9593145fb2e96f1353a307e1e04cf9fad5
Geheiner <timo.babst@gmail.com>
2015-04-27 12:14:40 +0200

    saturday1

--- index.php
+++ index.php
@@ -250,36 +250,28 @@ $newToken = generateToken();
 </div>
 </div>
 <div id="leaderboard">
-	<div class="tabs">
-		<ul class="tab-links">
-			<li class="active"><a href="#tab1">Tab #1</a></li>
-			<li><a href="#tab2">Tab #2</a></li>
-			<li><a href="#tab3">Tab #3</a></li>
-			<li><a href="#tab4">Tab #4</a></li>
-		</ul>
+<p>Language</p>
+<select id= "scoreLanguage" size="1">
+	<option>All languages</option>
+	<option>English</option>
+	<option>Vietnamese</option>
+</select>
+</br></br>
 
-		<div class="tab-content">
-			<div id="tab1" class="tab active">
-				<p>Tab #1 content goes here!</p>
-				<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis.</p>
-			</div>
+<p>Game</p>
+<select id= "scoreGame" size="1">
+	<option>All games</option>
+	<option>Game1</option>
+	<option>Game2</option>
+	<option>Game3</option>
+</select>
+</br></br>
+<p>Leader</p>
 
-			<div id="tab2" class="tab">
-				<p>Tab #2 content goes here!</p>
-				<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
-			</div>
+<div id="leader">
 
-			<div id="tab3" class="tab">
-				<p>Tab #3 content goes here!</p>
-				<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum ri.</p>
-			</div>
+</div>
 
-			<div id="tab4" class="tab">
-				<p>Tab #4 content goes here!</p>
-				<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
-			</div>
-		</div>
-	</div>
 </div>
 </div>
 </div>
