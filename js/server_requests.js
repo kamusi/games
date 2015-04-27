5f98c50559bec76a9a87c615cf4226856b37e846
Geheiner <timo.babst@gmail.com>
2015-04-27 17:01:28 +0200

    saturday1

--- js/server_requests.js
+++ js/server_requests.js
@@ -472,7 +472,7 @@ function get_user_stats() {
 
             document.getElementById('notifications').selectedIndex = whenToNotify 
             document.getElementById('posts').selectedIndex= whenToPost
-            document.getElementById('language').selectedIndex= language-1
+            document.getElementById('language').selectedIndex= (language-1)
 
             console.log("FUCK THIS IS THE FUCKING INDEX FCK "+ document.getElementById('language').selectedIndex)
             numSubmissions = obj.PositionMode1 + obj.PositionMode2 + obj.PositionMode3;
