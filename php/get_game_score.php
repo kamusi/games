e67d6e443e23cea569de9fb80f06319393fca039
Geheiner <timo.babst@gmail.com>
2015-04-29 22:02:08 +0200

    wed after martin

---
+++ php/get_game_score.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+include 'global.php';
+
+$user = 'root';
+$pass = '';
+$db = 'kamusi';
+
+
+$userID = $_GET['userID'];
+$language = $GET['language'];
+$mode = $GET['mode'];
+
+$returnText = "nothing";
+
+$mysqli = new mysqli('localhost', $user, $pass, $db);
+
+if (!$mysqli->set_charset('utf8')) {
+	printf("Error loading character set utf8: %s\n", $mysqli->error);
+}
+
+if(!in_array($mode, $acceptedModes)) {
+	die("Got a strange mode as input!". $mode);
+}
+$sql = "SELECT * FROM game" . $mode . " WHERE userid=? AND language = ?;"
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("si", $userID, $language );
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+$stmt->close();
+
+echo json_encode($row);
+
+?>
