d98af698530cf2ce6eed1171a7d972501b4c53c0
Geheiner <timo.babst@gmail.com>
2015-05-01 16:31:44 +0200

    friday

--- js/server_requests.js
+++ js/server_requests.js
@@ -299,8 +299,8 @@ function get_ranked() {
             definitionID = -1;
         }
     }
-    xmlhttp.open("GET","php/get_ranked.php?userID=" + userID + "&language=" + gameLanguage + "&mode=" +'1', true);
-//xmlhttp.open("GET","php/get_ranked_debug.php?userID=" + userID, true);
+//    xmlhttp.open("GET","php/get_ranked.php?userID=" + userID + "&language=" + gameLanguage + "&mode=" +'1', true);
+xmlhttp.open("GET","php/get_ranked_debug.php?userID=" + userID, true);
 
 xmlhttp.send();
 }
