e0ea7233083bb781f038ed1995dc6aa38cf89384
Geheiner <timo.babst@gmail.com>
2015-05-03 14:11:15 +0200

    friday

--- js/server_requests.js
+++ js/server_requests.js
@@ -615,7 +615,27 @@ function updateLeaderboard(){
 
     //All languages, All Games
 
+  var xmlhttp;
+  console.log("In update leaderboard")
+  
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            console.log("Leaderboard response : " + xmlhttp.responseText)
 
-    console.log("Language: " + lang)
+        }
+    }
+
+    xmlhttp.open("GET","php/get_user_rank.php?userID=" + userID + "&language=" + scoreLanguage + "&mode=" +scoreGame + "&metric=" + scoreMetric + "&period=" + scoretimePeriod , true);
+    xmlhttp.send();  
+
+
+
+
 
 }
--- php/get_user_rank.php
+++ php/get_user_rank.php
@@ -4,31 +4,36 @@ ini_set('display_errors', 'On');
 
 $userID = $_GET['userID'];
 $metric = $_GET['metric'];
-
+$gameMode = $_GET['mode'];
+$language = $_GET['language'];
+$timePeriod =  $_GET['period'];
 
 $user = 'root';
 $pass = '';
 $db = 'kamusi';
 
 
+//Get the 5 best players by rank according to the selected categories
+
 $mysqli = new mysqli('localhost', $user, $pass, $db);
 
 //Points
 if($metric == '0'){
 
-$stmt = $mysqli->prepare("");
-$stmt->bind_param("ssssii",  $tweetID,$tweetText, $tweetAuthor,$userID, $wordID, $good);
-
-$stmt->execute();
-$stmt->close();
-	
+	//rank over everything
+	if($language == '0' && $mode == '0'){
+		$sql = "SELECT SUM(t.points) AS totalpoints FROM (SELECT points FROM game1 WHERE userid=? UNION ALL";
+		$sql .= " SELECT points FROM game2 WHERE userid=? UNION ALL SELECT points FROM game3 WHERE userid=?";
+		$sql .= " ) t";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->bind_param("sss",$userID,$userID,$userID);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row = $result->fetch_assoc();
+		$totalScore = $row["totalpoints"];
+		$stmt->close();		
+	}
 }
-
-$stmt = $mysqli->prepare("");
-$stmt->bind_param("ssssii",  $tweetID,$tweetText, $tweetAuthor,$userID, $wordID, $good);
-
-$stmt->execute();
-$stmt->close();
 
 
 echo "Total Score : " . $totalScore;
