4b2625ed735c8bcb268716e28c829dd01c94b469
Geheiner <timo.babst@gmail.com>
2015-05-10 15:12:06 +0200

    does this work en the server

--- php/get_swahiliSentences.php
+++ php/get_swahiliSentences.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 $keyword = $_GET['keyword'];
 $amount = $_GET['amount'];
 
-/*
+
 set_include_path(get_include_path() . get_include_path().'/phpseclib');
 include('Net/SSH2.php');
 
@@ -15,16 +15,16 @@ if (!$ssh->login('babst', 'Jsts8472')) {
 
 echo $ssh->exec('pwd');
 echo $ssh->exec('ls -la');
-*/
 
 
 
+/*
 #$output = shell_exec('cd .. ; echo $USER ; ssh -i /home/ec2-user/.ssh/taitoApache.rsa babst@taito.csc.fi  \'bash -s\' < getDataForWord.sh ' . $keyword . " " . $amount . " 2>&1");
-$output = shell_exec('cd .. ; echo $USER ; ssh -i /var/www/.ssh/taitoApache.rsa  babst@taito.csc.fi echo bouyeah ls 2>&1');
+$output = shell_exec('cd .. ; echo $USER ; ssh -i /var/www/.ssh/taitoApache.rsa babst@taito.csc.fi echo bouyeah ls 2>&1');
 
 #$output = shell_exec("cd .. ; pwd;  bash getDataForWord.sh kamusi 5 2>&1");# . $keyword . " " . $amount);
 #$output = shell_exec('echo blabla; groups $USER; echo $USER');
-
+*/
 echo($output);
 
 ?>
