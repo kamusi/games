f3f113772abf63505ef44174002d51e9a1a18024
Geheiner <timo.babst@gmail.com>
2015-05-20 15:50:05 +0200

    wednesday

--- php/global.php
+++ php/global.php
@@ -3,9 +3,9 @@
 $acceptedModes = array("1","2","3","4");
 $allUsers = "allusers";
 
-$e1= _("Definition Game");
+$e1=_("Definition Game");
 $e2=_("Translation Game");
-$e3= _("Tweet Game");
+$e3= ("Tweet Game");
 $e4=_("Sentence Game");
 $gameNames = array('1' => $e1, '2' => $e2 , '3' => $e3, '4'=> $e4);
 
