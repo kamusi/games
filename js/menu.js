38e121505d491381cd6cc199ffb223100f2a56d8
Geheiner <timo.babst@gmail.com>
2015-05-28 15:44:01 +0200

    thursday

--- js/menu.js
+++ js/menu.js
@@ -231,6 +231,7 @@ function display_profile() {
 
 function display_changeLanguage() {
 		document.getElementById("profile").style.display = "none";
+			document.getElementById("settings").style.display = "none";
 	document.getElementById("changeMenuLanguage").style.display = "inline-block";	
 }
 
