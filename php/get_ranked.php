49027f132988f2b1fc55ad9792bc404422528198
Geheiner <timo.babst@gmail.com>
2015-06-03 16:56:41 +0200

    wednesday

--- php/get_ranked.php
+++ php/get_ranked.php
@@ -42,7 +42,7 @@ function lookForWord($userID) {
 //fetch the word that has as rank user s position+offset
 	$sql =  "SELECT ID As ID, DefinitionID As DefinitionID, Rank As Rank FROM (";
 		$sql.=	"SELECT w.ID, w.DefinitionID, r.Rank FROM rankedwords As r LEFT JOIN words As w ON r.Word = w.Word";
-		$sql.=	") As sq WHERE sq.ID IS NOT NULL AND sq.DefinitionID IS NOT NULL AND sq.ID NOT IN (SELECT wordid FROM seengames WHERE (userid=? OR userid=?) AND game=? ) AND sq.Rank = ?;";
+		$sql.=	") As sq WHERE sq.ID IS NOT NULL AND sq.DefinitionID IS NOT NULL AND sq.ID NOT IN (SELECT wordid FROM seengames WHERE (userid=? OR userid=?) AND game=?) AND sq.Rank = ?;";
 
 $sum = intval($user_position) + intval($user_offset);
 
