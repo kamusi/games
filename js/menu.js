0b4b586f90ccc31788cd1b77eb32278ef2431054
Geheiner <timo.babst@gmail.com>
2015-06-08 12:26:04 +0200

    monday meeting

--- js/menu.js
+++ js/menu.js
@@ -32,7 +32,7 @@ function InlineEditorController($scope){
 				$scope.value = '';
 			}
 
-			$("#input_tool_box").trigger("focus");
+			$("#input_tool_box").focus();
 			document.getElementById("input_tool_box").select();
 		}
 
