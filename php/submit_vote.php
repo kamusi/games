776d06ef039944c5b5c130e44443c66940028094
Geheiner <timo.babst@gmail.com>
2015-06-10 17:28:15 +0200

    wednesday

--- php/submit_vote.php
+++ php/submit_vote.php
@@ -4,9 +4,8 @@ include 'notification.php';
 include 'login_services.php';
 
 //login($sess_user, $sess_pass, $base_url);
-getToken($kamusiUser['session_name'], $kamusiUser['session_id'], $base_url);
 
-echo "END 1";
+echo "START";
 var_dump($kamusiUser);
 login($sess_user, $sess_pass, $base_url);
 echo "END 2";
