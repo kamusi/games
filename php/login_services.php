9cf78fe19c9266159f5301854c55767878cc9b0c
Geheiner <timo.babst@gmail.com>
2015-06-11 13:25:02 +0200

    thursday

--- php/login_services.php
+++ php/login_services.php
@@ -23,10 +23,10 @@ function connect($session_name, $session_id, $csrf_token, $base_url) {
 	$ch = curl_init();
 
 	setCurlDefaults($ch,$base_url);
-//	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, TRUE);
 	curl_setopt($ch, CURLOPT_URL, $base_url . "/facebook_game_v1/system/connect.json");
 
-//	curl_setopt($ch, CURLOPT_HTTPHEADER, $connectHeaders);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $connectHeaders);
 
 	$result = json_decode(curl_exec($ch)); 
 
@@ -85,7 +85,7 @@ function getToken($session_name, $session_id, $base_url) {
 	setCurlDefaults($ch,$base_url);
 
 	if($session_name != '' && $session_id != '' ){
-		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	//	curl_setopt($ch, CURLOPT_HEADER, TRUE);
 		$headers = array();
 		$headers['Cookie'] = $session_name . "=" . $session_id;
 
