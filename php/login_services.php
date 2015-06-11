028d064ccca2b713cb167fb448b15d2613c4b2f9
Geheiner <timo.babst@gmail.com>
2015-06-11 13:59:35 +0200

    thursday

--- php/login_services.php
+++ php/login_services.php
@@ -23,6 +23,7 @@ function connect($session_name, $session_id, $csrf_token, $base_url) {
 
 	setCurlDefaults($ch,$base_url);
 //	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 
 	curl_setopt($ch, CURLOPT_URL, $base_url . "/facebook_game_v1/system/connect.json");
 
