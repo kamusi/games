f735704ceec05f29ef187cd3281ab1d37aea0808
Geheiner <timo.babst@gmail.com>
2015-06-11 12:36:00 +0200

    thursday

--- php/login_services.php
+++ php/login_services.php
@@ -83,13 +83,17 @@ function getToken($session_name, $session_id, $base_url) {
 
 	$ch = curl_init();
 	setCurlDefaults($ch,$base_url);
+
+	if($session_name != '' && $session_id != '' ){
 	curl_setopt($ch, CURLOPT_HEADER, TRUE);
-
-	curl_setopt($ch, CURLOPT_URL, $base_url . "/facebook_game_v1/user/token.json");
-
 	$headers = array();
 	$headers['Cookie'] = $session_name + "=" + $session_id;
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+}
+
+	curl_setopt($ch, CURLOPT_URL, $base_url . "/facebook_game_v1/user/token.json");
+
+
 
 	//set xhrfields : with credentials?
 
