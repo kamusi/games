250d3125563e9922c198d3136ef6401e2a6e1717
Geheiner <timo.babst@gmail.com>
2015-06-11 13:25:52 +0200

    thursday

--- php/login_services.php
+++ php/login_services.php
@@ -17,6 +17,7 @@ function connect($session_name, $session_id, $csrf_token, $base_url) {
 	$connectHeaders = array();
 	if ($csrf_token !== '' || $csrf_token !== 'undefined') {
 		$connectHeaders['X-CSRF-Token'] = $csrf_token;
+		echo "Here are the connect headers";
 		var_dump($connectHeaders);
 	};
 
