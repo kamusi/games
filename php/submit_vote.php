f0b60778af6a2718e22b847213af52f2604ab534
Geheiner <timo.babst@gmail.com>
2015-06-12 10:49:45 +0200

    friday

--- php/submit_vote.php
+++ php/submit_vote.php
@@ -26,10 +26,9 @@ getToken($kamusiUser['session_name'], $kamusiUser['session_id'], $base_url);
 connect($kamusiUser['session_name'], $kamusiUser['session_id'], $kamusiUser['csrf_token'], $base_url);
 echo "END 4";
 var_dump($kamusiUser);
-/*
 connect($kamusiUser['session_name'], $kamusiUser['session_id'], $kamusiUser['csrf_token'], $base_url);
-var_dump($kamusiUser);
-*/
+
+
 $wordID = $_GET['wordID'];
 $definitionID = $_GET['definitionID'];
 $vote = $_GET['vote'];
