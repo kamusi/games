65a2ab76ef7d4eb05df69a26d0cadf3111d465f0
Geheiner <timo.babst@gmail.com>
2015-06-12 14:05:36 +0200

    friday

--- php/login_services.php
+++ php/login_services.php
@@ -212,7 +212,7 @@ function getSwahiliwords($uid){
 
 
 
-	$plainResult = authenticatedGETRequest("/facebook_game_v1/search-define.json?to_language=371&page=90");
+	$plainResult = authenticatedGETRequest("/facebook_game_v1/search-define.json?to_language=371&page=150");
 	debugVariable($plainResult, 'words: ');
 }
 
