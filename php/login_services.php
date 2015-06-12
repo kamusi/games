96e35fe2024c0066f1e7758fa7207c0c9419db93
Geheiner <timo.babst@gmail.com>
2015-06-12 15:44:14 +0200

    friday

--- php/login_services.php
+++ php/login_services.php
@@ -230,10 +230,10 @@ function getSwahiliwords($uid){
 //	debugVariable($pageNumber, 'page numebr');
 
 	$json = json_decode($plainResult, true);
-	debugVariable($json[$pageEntry], 'words: ' );
+//	debugVariable($json[$pageEntry], 'words: ' );
 
 	//What we will return to the javascript
-	json_encode($json[$pageEntry], true);
+	echo json_encode($json[$pageEntry]);
 
 }
 
