fc8092d52d836cdf90cb18813f873c02374b29e5
Geheiner <timo.babst@gmail.com>
2015-06-13 14:59:48 +0200

    sat

--- index.php
+++ index.php
@@ -235,24 +235,24 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
 <div id="info1">
 			<div id="info1-main">
 				<div id="bla">
-					<p><h1><strong>Definition Game</strong></h1><br />
-  Write a definition: 1 point<br />
-  Vote for the winning definition: 1 point<br />
-  <em><strong>Write the winning definition</strong></em>: 5 points + credit on the dictionary entry page<br />
-  Submit a <em>spam</em> definition: -10 points + watchlist</p>
-<p><strong>How to write a great definition</strong><br />
-  Definitions are explanations of what a word means. They are not single words (those are synonyms). You can usually use a definition instead of the actual word. </p>
-<p>Definitions are NOT translations. They are written in the same language as the term you are defining. If you are defining a word in your language, the definition should be in that language, too. Definitions should define what the term means in your language – the meaning may differ somewhat from the English definition we show of a matched English concept!</p>
-<p>Stick to these rules:</p>
+					<p><h1><strong><?php echo _("Definition Game"); ?></strong></h1><br />
+  <?php echo _("Write a definition: 1 point"); ?><br />
+  <?php echo _("Vote for the winning definition: 1 point"); ?><br />
+  <em><strong><?php echo _("Write the winning definition"); ?></strong></em><?php echo _(": 5 points + credit on the dictionary entry page"); ?><br />
+  <?php echo _("Submit a"); ?> <em><?php echo _("spam"); ?></em> <?php echo _(" definition: -10 points + watchlist"); ?></p>
+<p><strong><?php echo _("How to write a great definition"); ?></strong><br />
+  <?php echo _("Definitions are explanations of what a word means. They are not single words (those are synonyms). You can usually use a definition instead of the actual word."); ?> </p>
+<p><?php echo _("Definitions are NOT translations. They are written in the same language as the term you are defining. If you are defining a word in your language, the definition should be in that language, too. Definitions should define what the term means in your language – the meaning may differ somewhat from the English definition we show of a matched English concept!"); ?></p>
+<p><?php echo _("Stick to these rules:"); ?></p>
 <ul type="disc">
-  <li>Short and sweet: A definition should be as brief as possible to explain the concept, but long enough to describe it fully. If the same word has <strong><em>different meanings</em></strong>, those are <strong><em>different concepts</em></strong> with different definitions. <strong><em>A definition</em></strong> in Kamusi only explains <strong><em>one concept</em></strong>.</li>
-  <li>Easy does it: Use the simplest words you can. Definitions should not force readers to jump around the dictionary more, unless technical terms are absolutely necessary.</li>
-  <li>No circles: Definitions should NOT contain the word that is being defined, nor its close relatives. <em>Happiness</em> is "A feeling of joy.", not "The feeling of being happy." Definitions should not be circular - we cannot now say <em>joy</em> is "A feeling of happiness."</li>
-  <li>No fluff: Do not start with "A term meaning", or "This is a", or "X refers to", etc. Style: Begin with a Capital letter and end with a period.</li>
+  <li><?php echo _("Short and sweet: A definition should be as brief as possible to explain the concept, but long enough to describe it fully. If the same word has"); ?> <strong><em><?php echo _("different meanings"); ?></em></strong>, <?php echo _("those are"); ?><strong><em> <?php echo _("different concepts"); ?></em></strong> <?php echo _("with different definitions."); ?> <strong><em><?php echo _("A definition"); ?></em></strong> <?php echo _("in Kamusi only explains "); ?><strong><em><?php echo _("one concept"); ?></em></strong>.</li>
+  <li><?php echo _("Easy does it: Use the simplest words you can. Definitions should not force readers to jump around the dictionary more, unless technical terms are absolutely necessary."); ?></li>
+  <li><?php echo _("No circles: Definitions should NOT contain the word that is being defined, nor its close relatives."); ?> <em><?php echo _("Happiness"); ?></em> <?php echo _(" is A feeling of joy. "); ?> <?php echo _('not "The feeling of being happy." Definitions should not be circular - we cannot now say); ?> <em><?php echo _('joy'); ?></em> <?php echo _('is "A feeling of happiness."); ?></li>
+  <li><?php echo _('No fluff: Do not start with "A term meaning", or "This is a", or "X refers to", etc. Style: Begin with a Capital letter and end with a period.'); ?></li>
 </ul>
 <p><br />
-  For <strong>English</strong>, we have 200,000 entries with "working definitions" borrowed from the <a href="http://wordnet-rdf.princeton.edu" target="_blank">Princeton WordNet</a>. Some of these definitions are great, but many can be much better. When you improve the definitions in English, we can provide better guidance for translations across languages.</p>
-<p>For more insight about writing definitions, you can <a href="https://www.youtube.com/watch?v=aaqOQQOYuHA" target="_blank">watch this video</a>.</p>
+  <?php echo _("For"); ?> <strong><?php echo _("English"); ?></strong><?php echo _(', we have 200,000 entries with "working definitions" borrowed from the'); ?> <a href="http://wordnet-rdf.princeton.edu" target="_blank">Princeton WordNet</a>. <?php echo _('Some of these definitions are great, but many can be much better. When you improve the definitions in English, we can provide better guidance for translations across languages.'); ?></p>
+<p><?php echo _("For more insight about writing definitions, you can "); ?><a href="https://www.youtube.com/watch?v=aaqOQQOYuHA" target="_blank"><?php echo _("watch this video"); ?></a>.</p>
 	</div>
 	<div class="info-footer">
 		<img title="Return" class="control" src="media/leftarrow.png" onclick="playClick();return_to_game(); ">
@@ -262,7 +262,7 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
 <div id="info2">
 			<div id="info2-main">
 				<div class="bla">
-<p><h1>Translation Game</h1><br />
+<p><h1><strong>Translation Game</strong></h1><br />
   Submit a term that becomes the consensus: 3 points<br />
   Skip a term where the consensus is "skip" for this language: 1 point</p>
 <p>When we do not already have an equivalent term in your language for an idea that we have in our English dataset, we ask you to tell us the best way to express the same meaning.</p>
