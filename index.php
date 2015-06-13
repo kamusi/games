b3c99fb005a7b245ae0055fa1dfe2452edc88e7f
Geheiner <timo.babst@gmail.com>
2015-06-13 15:20:24 +0200

    sat

--- index.php
+++ index.php
@@ -268,27 +268,27 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
 <div id="info3">
 			<div id="info3-main">
 				<div class="bla">
-<p><h1><strong>Examples Games (Tweets and Sentences)</strong> </h1><br />
-  Skip an example where the consensus is "skip": 1 point<br />
-  Select an example where the consensus is "select": 1 point</p>
-<p>Usage examples help people understand how a term is really used. Finding a good example can be hard, however.</p>
+<p><h1><strong><?php echo _('Examples Games (Tweets and Sentences)'); ?></strong> </h1><br />
+  <?php echo _('Skip an example where the consensus is "skip": 1 point'); ?><br />
+  <?php echo _('Select an example where the consensus is "select": 1 point'); ?></p>
+<p><?php echo _('Usage examples help people understand how a term is really used. Finding a good example can be hard, however.'); ?></p>
 <ul>
-  <li>Bad: "Your eyes are blue."</li>
-  <li>Good: "Your eyes are the same blue as the water in the bay, and sparkle even more in the sunlight."</li>
+  <li><?php echo _('Bad: "Your eyes are blue."'); ?></li>
+  <li><?php echo _('Good: "Your eyes are the same blue as the water in the bay, and sparkle even more in the sunlight."'); ?></li>
 </ul>
-<p>Because words have different senses, an example might be excellent in one context but completely wrong in another.</p>
+<p><?php echo _('Because words have different senses, an example might be excellent in one context but completely wrong in another.'); ?></p>
 <ul>
-  <li>Good example for a different meaning of <strong><em>blue</em></strong>: "My heart is aching from the pain of missing you, I'm feeling so blue."</li>
+  <li><?php echo _('Good example for a different meaning of'); ?> <strong><em><?php echo _('blue'); ?></em></strong>: <?php echo _('"My heart is aching from the pain of missing you, I am feeling so blue."'); ?></li>
 </ul>
-<p>In the Examples games, your goal is to find sentences that are real gems for a specific meaning of a term. The Tweet game shows you a bunch of recent postings to Twitter, while the Sentences game grabs examples from other sources. You read the definition, and click on the tweets or sentences that will be a great help for dictionary users to understand the exact meaning we show.</p>
+<p><?php echo _('In the Examples games, your goal is to find sentences that are real gems for a specific meaning of a term. The Tweet game shows you a bunch of recent postings to Twitter, while the Sentences game grabs examples from other sources. You read the definition, and click on the tweets or sentences that will be a great help for dictionary users to understand the exact meaning we show.'); ?></p>
 <ul>
-  <li>Do NOT click on an entry if it is a good example of a <em>different</em> meaning.</li>
-  <li>Do NOT click on an entry if it kind-of, sort-of works with the meaning in question.</li>
-  <li>ONLY click on an entry if it <strong><em>perfectly demonstrates</em></strong> the meaning <strong><font color="#EBA634">written in orange</font></strong>.</li>
+  <li><?php echo _('Do NOT click on an entry if it is a good example of a'); ?> <em><?php echo _('different'); ?></em> <?php echo _('meaning'); ?>.</li>
+  <li><?php echo _('Do NOT click on an entry if it kind-of, sort-of works with the meaning in question.'); ?></li>
+  <li><?php echo _('ONLY click on an entry if it'); ?> <strong><em><?php echo _('perfectly demonstrates'); ?></em></strong> <?php echo _('the meaning'); ?> <strong><font color="#EBA634"><?php echo _('written in orange'); ?></font></strong>.</li>
 </ul>
-<p>Remember, you will score points if you and others all agree to "skip", and you will score points if you and others all agree to "select". </p>
-<p><strong>Play at your own risk </strong><br />
-  We try to filter out inappropriate content in advance. Please be warned, however, that some objectionable content might slip through our automated filters. We apologize in advance for the possibility that you might see offensive Tweets. We do not have humans reviewing the content you see in the Examples games – YOU are the filter. By ignoring offensive examples, you will help make them vanish from our data collection.</p>
+<p><?php echo _('Remember, you will score points if you and others all agree to "skip", and you will score points if you and others all agree to "select". '); ?></p>
+<p><strong><?php echo _('Play at your own risk'); ?> </strong><br />
+  <?php echo _('We try to filter out inappropriate content in advance. Please be warned, however, that some objectionable content might slip through our automated filters. We apologize in advance for the possibility that you might see offensive Tweets. We do not have humans reviewing the content you see in the Examples games – YOU are the filter. By ignoring offensive examples, you will help make them vanish from our data collection.'); ?></p>
   	</div>
 	<div class="info-footer">
 		<img title="Return" class="control" src="media/leftarrow.png" onclick="playClick();return_to_game(); ">
