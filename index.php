34341efe4df464589212050e5893ad69aefaeaa1
Geheiner <timo.babst@gmail.com>
2015-06-13 15:10:33 +0200

    sat

--- index.php
+++ index.php
@@ -205,16 +205,16 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
 		<div id="about">
 			<div id="about-main">
 				<div id="about1">
-<p><strong><h1>About</h1></strong><br />
-  The Kamusi Project (<a href="http://kamusi.org/">http://kamusi.org/</a>) is a participatory international effort dedicated to improving knowledge of the world's languages. Our long term mission is to produce dictionaries and other language resources for every language, and to make those resources available for free to everyone.</p>
-<p><strong>Purpose</strong><br />
-  Our games are designed to learn all about your language, from you, so that we can share that knowledge with the rest of the world. Different games seek different pieces of information. What we learn in one game builds information we can use in the next. Our goal is a detailed dictionary for your language, linked to translations for all other languages, available to you for free.</p>
-<p><strong>Play</strong><br />
-  Our games ask you a variety of types of questions. You earn points when your answer agrees with the consensus. Your points will remain <strong><em>in play</em></strong> until consensus, when they are <strong><em>banked</em></strong> if you are on the winning team. If you do not have an answer, just skip to the next question! When enough people agree to the same answer, it is published to the live Kamusi. For definitions, the winning author will receive credit on the entry page.</p>
-<p><strong>Honor Code</strong><br />
-  We ask each player to give their one best answer to each question. Please do not attempt to game the game, and please alert us to suspicious items so we can remove spammers and all their spam.</p>
-<p><strong>Friends</strong><br />
-  The more people who play for your language, the more fun you will have – you score points more quickly, and move toward more advanced questions. If you enjoy playing, please tell your friends to join us! They&rsquo;ll have fun too, and together you will contribute to a great resource for your whole community.</p>		</div>
+<p><strong><h1><?php echo _('About'); ?></h1></strong><br />
+  <?php echo _('The Kamusi Project ('); ?><a href="http://kamusi.org/">http://kamusi.org/</a>) <?php echo _('is a participatory international effort dedicated to improving knowledge of the world s languages. Our long term mission is to produce dictionaries and other language resources for every language, and to make those resources available for free to everyone.'); ?></p>
+<p><strong><?php echo _('Purpose'); ?></strong><br />
+  <?php echo _('Our games are designed to learn all about your language, from you, so that we can share that knowledge with the rest of the world. Different games seek different pieces of information. What we learn in one game builds information we can use in the next. Our goal is a detailed dictionary for your language, linked to translations for all other languages, available to you for free.'); ?></p>
+<p><strong><?php echo _('Play'); ?></strong><br />
+  <?php echo _('Our games ask you a variety of types of questions. You earn points when your answer agrees with the consensus. Your points will remain '); ?><strong><em><?php echo _('in play'); ?></em></strong> <?php echo _('until consensus, when they are '); ?><strong><em><?php echo _('banked'); ?></em></strong> if you are on the winning team. If you do not have an answer, just skip to the next question! When enough people agree to the same answer, it is published to the live Kamusi. For definitions, the winning author will receive credit on the entry page.'); ?></p>
+<p><strong><?php echo _('Honor Code'); ?></strong><br />
+  <?php echo _('We ask each player to give their one best answer to each question. Please do not attempt to game the game, and please alert us to suspicious items so we can remove spammers and all their spam.'); ?></p>
+<p><strong><?php echo _('Friends'); ?></strong><br />
+  <?php echo _('The more people who play for your language, the more fun you will have – you score points more quickly, and move toward more advanced questions. If you enjoy playing, please tell your friends to join us! They will have fun too, and together you will contribute to a great resource for your whole community.'); ?></p>		</div>
 	</div>
 	<div id="about-footer">
 		<img title="Return" class="control" src="media/leftarrow.png" onclick="playClick();return_to_game(); ">
