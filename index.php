9fa06cff19e3905bdd3c2fdbc61b42701839c319
Geheiner <timo.babst@gmail.com>
2015-06-17 13:41:20 +0200

    tee

--- index.php
+++ index.php
@@ -251,16 +251,17 @@ $partOfSpeechArray= array('noun' => _("noun"), 'verb' => _('verb'), 'adjective' 
 <div id="info2">
 			<div id="info2-main">
 				<div class="bla">
-<p><h1><strong><?php echo _('Translation Game'); ?></strong></h1><br />
-  <?php echo _('Submit a term that becomes the consensus: 3 points'); ?><br />
-  <?php echo _('Skip a term where the consensus is "skip" for this language: 1 point'); ?></p>
-<p><?php echo _('When we do not already have an equivalent term in your language for an idea that we have in our English dataset, we ask you to tell us the best way to express the same meaning.'); ?></p>
+<?php echo _(' <p><strong>Translation Game</strong><br />'); ?>
+<?php echo _('   Submit a term that becomes the consensus: 3 points<br />'); ?>
+<?php echo _('   Skip a term where the consensus is "skip" for this language: 1 point</p>'); ?>
+<?php echo _(' <p>When we do not already have an equivalent term in your language for an idea that we have in our English dataset, we ask you to tell us the best way to express the same meaning.</p>'); ?>
 <ul>
-  <li><?php echo _('You may not be able to give any translation. For example, many languages do not have a term for <strong><em>run</em></strong>, "a point in the game of baseball". If you cannot translate a term, <strong>skip</strong> to the next.'); ?></li>
-  <li><?php echo _('Your term may not have exactly the same meaning as the English term. For example, English has 10 <strong><em>fingers</em></strong> and 10 <strong><em>toes</em></strong>, while Spanish has 20 <strong><em>dedos</em></strong>. For the translation game, just give us the closest equivalent, and we will figure out non-parallel meanings in a different game.'); ?></li>
-  <li><?php echo _('Your language might have more than one word that means the same thing. For example, you might use the words'); ?> <strong><em><?php echo _('sofa'); ?></em></strong> <?php echo _('and'); ?> <strong><em><?php echo _('couch'); ?></em></strong> <?php echo _('interchangeably. For the translation game, just tell us your favorite. We will figure out synonyms in a different game, and you will earn your points if you provide an answer that eventually reaches consensus.'); ?></li>
+<?php echo _('   <li>You may not be able to give any translation. For example, many languages do not have a term for <strong><em>run</em></strong>, "a point in the game of baseball". If you cannot translate a term, <strong>skip</strong> to the next.</li>'); ?>
+<?php echo _('   <li>Your term may not have exactly the same meaning as the English term. For example, English has 10 <strong><em>fingers</em></strong> and 10 <strong><em>toes</em></strong>, while Spanish has 20 <strong><em>dedos</em></strong>. For the translation game, just give us the closest equivalent, and we\'ll figure out non-parallel meanings in a different game.</li>'); ?>
+<?php echo _('   <li>Your language might have more than one word that means the same thing. For example, you might use the words <strong><em>sofa</em></strong> and <strong><em>couch</em></strong> interchangeably. For the translation game, just tell us your favorite. We\'ll figure out synonyms in a different game, and you\'ll earn your points if you provide an answer that eventually reaches consensus.</li>'); ?>
 </ul>
-	</div>
+
+</div>
 	<div class="info-footer">
 		<img title="Return" class="control" src="media/leftarrow.png" onclick="playClick();return_to_game(); ">
 	</div>
